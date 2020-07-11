53b251ac3a7b25ded26032f340f74937c45da98e
VetoMrm <manjunath.r@vetologic.com>
2020-07-11 11:21:08 +0530

    homepage is done

---
+++ Js/demo.js
@@ -0,0 +1,18 @@
+// some scripts
+
+
+
+
+
+
+//////////////////////// Bootstrap tooltip
+if ($('[data-toggle="tooltip"]').length > 0) { // check if element exists
+    $('[data-toggle="tooltip"]').tooltip()
+} // end if
+
+
+
+
+
+});
+// jquery end
